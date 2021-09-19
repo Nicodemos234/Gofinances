5fb48924c107e08ac0d692587ec53fa6fcd60965
nicodemos234 <nicodemos54324@gmail.com>
2021-09-19 16:32:59 -0300

    Add hightlight cards and header

--- App.tsx
+++ App.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+
+import {
+useFonts,
+Poppins_400Regular,
+Poppins_500Medium,
+Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme';
 import Dashboard from './src/Screens/Dashboard';
 
 export default function App() {
-  return <Dashboard />;
+  const [fontsLoader] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if(!fontsLoader){
+    return <AppLoading />
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,10 +1,58 @@
 import React from 'react'
-import { Container, Title } from './styles'
+import HighlightCard from '../../components/HighlightCard'
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards
+} from './styles'
 
 export default function Dashboard() {
-    return (
-        <Container>
-            <Title>Dashboard</Title>
-        </Container>
-    )
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/35513987?v=4' }} />
+            <User>
+              <UserGreeting>
+                Olá,
+              </UserGreeting>
+              <UserName>
+                Nicodemos
+              </UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
+    </Container>
+  )
 }
